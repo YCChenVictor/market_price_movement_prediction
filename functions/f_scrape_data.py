f5dd8a7617b3fc96e14aa47f7206c88f1248dc21
Victor Chen <victor.chen@hoc-trade.com>
2024-09-07 00:47:42 +0800

    refactor: Add function to get historical price data with yfinance

--- functions/f_scrape_data.py
+++ functions/f_scrape_data.py
@@ -1,6 +1,10 @@
 from time import sleep
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+import sys
+import yfinance as yf
+import json
+import pprint
 
 def download_yahoo_quotes(ticker, period_seconds, driver):
     """
@@ -12,3 +16,15 @@ def download_yahoo_quotes(ticker, period_seconds, driver):
                '&interval=1d&events=history'
                .format(ticker, period_seconds))
     sleep(1)
+
+def get_historical_price_with_yfinace(symbol):
+    ticker = yf.Ticker(symbol)
+    historical_prices = ticker.history(period='1d', interval="1m")
+    historical_price_data = [
+        {
+            **{'time': index.isoformat(), 'symbol': symbol},
+            **row[['Open', 'High', 'Low', 'Close']].to_dict()
+        }
+        for index, row in historical_prices.iterrows()
+    ]
+    return historical_price_data
