ff2244ad54835c6bc2e9afcb4978c916ce6993d2
Victor Chen <victor.chen@hoc-trade.com>
2024-10-06 22:32:11 +0800

    chore: remove unnecessary comments

--- src/prediction.py
+++ src/prediction.py
@@ -1,55 +1,18 @@
-# conclusion:
-# 1. CNN cannot be used (training accuracy cannot increase)
-# 2. LSTM can still be used (training accuracy can reach 100%)
-# 3. Convert CNN_LSTM to ConvLSTM
+# Process:
+# scrape finance data from yahoo finance
+# modify data to deal with NAN and repeat data problem
+# calculate volatility
+# calculate movements
+# calculate rolling connectedness
+# add rolling connectedness (used only when there is new connectedness)
+# Turn data into TFRecord format
+# train model (CNN, RNN, ConvLSTM)
+# predict movement (CNN, RNN, ConvLSTM)
 
-# problem:
-# 1. Change CNN_LSTM to ConvLSTM
-# 2. Visualize loss and accuracy
-# 3. For LSTM prediction, predict the entire batch. Good news is that TensorFlow can share weights, so you can save the weights and use them in another graph to predict batch = 1.
-
-# batch, epoch & iteration
-"""
-epoch: When a complete dataset passes through a neural network and returns once, this process is called one epoch.
-batch: When it is not possible to pass the entire dataset through the neural network at once, the dataset is divided into several batches.
-iteration: An iteration is the number of times a batch needs to be completed to complete one epoch.
-
-For example, if there is a dataset with 2000 training samples and it is divided into batches of size 500, then it would take 4 iterations to complete one epoch.
-"""
-
-# Procedure:
-# 1. scrape finance data from yahoo finance
-# 2. scrape finance data from investing.com (now manually)
-# 3. modify data to deal with NAN and repeat data problem
-# 4. calculate volatility
-# 5. calculate movements (should have the option to select label or value in the future)
-# 6. calculate rolling connectedness (should try to increase the speed in the future)
-# 7. add rolling connectedness (used only when there is new connectedness)
-# 8. Turn data into TFRecord format
-# 9. train model (CNN, RNN, ConvLSTM)
-# 10. predict movement (CNN, RNN, ConvLSTM)
-
-# import required module
 import json
 import os
 import time
 import functions.f_about_path as fap
-
-# count the elapsed time span, start time
-start_time = time.time()
-
-"""
-the relationship bet. batch, epoches:
-"""
-
-with open("prerequisite.json", "r") as f:
-    prerequisite = json.load(f)
-
-# get the number of market prices and the num of elements of connectedness table
-target_folder = prerequisite["target_folder"]
-save_path = "./docs/" + target_folder
-num_of_files = sum([len(files) for r, d, files in os.walk(save_path)])
-num_table_elements = (num_of_files + 1) ** 2
 
 # scrape finance data from yahoo finance
 print("scraping finance data")
