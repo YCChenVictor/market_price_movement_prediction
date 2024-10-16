bbc05100fdca9db6d3c7abef7a7165d95bd47395
Victor Chen <victor.chen@hoc-trade.com>
2024-10-16 22:19:30 +0800

    test: add test for scraping data

--- src/scrape_finance_data_yahoo.py
+++ src/scrape_finance_data_yahoo.py
@@ -5,29 +5,38 @@ import asyncio
 
 
 def get_historical_price_with_yfinace(symbol):
-    ticker = yf.Ticker(symbol)
-    historical_prices = ticker.history(period="1d", interval="1m")
-    historical_price_data = [
-        {
-            **{"time": index.isoformat(), "symbol": symbol},
-            **row[["Open", "High", "Low", "Close"]].to_dict(),
-        }
-        for index, row in historical_prices.iterrows()
-    ]
-    return historical_price_data
+    try:
+        ticker = yf.Ticker(symbol)
+        historical_prices = ticker.history(period="1d", interval="1m")
+        historical_price_data = [
+            {
+                **{"time": index.isoformat(), "symbol": symbol},
+                **row[["Open", "High", "Low", "Close"]].to_dict(),
+            }
+            for index, row in historical_prices.iterrows()
+        ]
+        return historical_price_data
+    except Exception as e:
+        print(f"Error getting data from yfinance with {symbol}: {e}")
+        raise
+
+
+async def fetch_and_save(symbol, directory):
+    try:
+        result = get_historical_price_with_yfinace(symbol)
+        pd.DataFrame(result).to_csv(f"{directory}/{symbol}.csv")
+    except Exception as e:
+        print(f"Error saving data with {symbol}: {e}")
+        raise
 
 
 async def scrape_and_save_data(symbols, directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    async def fetch_and_save(symbol):
-        try:
-            result = get_historical_price_with_yfinace(symbol)
-            pd.DataFrame(result).to_csv(f"{directory}/{symbol}.csv")
-        except Exception as e:
-            print(f"Error processing {symbol}: {e}")
+        # Use asyncio.gather to run tasks concurrently
+        await asyncio.gather(*[fetch_and_save(symbol, directory) for symbol in symbols])
+    except Exception as e:
+        print(f"Error scraping and saving data with {symbols}: {e}")
         raise
-
-    # Use asyncio.gather to run tasks concurrently
-    await asyncio.gather(*[fetch_and_save(symbol) for symbol in symbols])
