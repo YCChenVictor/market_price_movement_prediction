e503d79f529dccb09adeb869d878ed777d23d856
Victor Chen <victor.chen@hoc-trade.com>
2024-10-18 15:02:09 +0800

    feature: finish predict script based on timestamp

--- scrape_train_data.py
+++ scrape_train_data.py
@@ -5,6 +5,9 @@ from market_price_movement_prediction.etl import ETL
 # Define arguments
 tickers = ['AUDCAD=X', 'AUDCHF=X', 'AUDJPY=X', 'AUDNZD=X', 'CADCHF=X', 'CADJPY=X', 'CHFJPY=X']
 train_data_dir = 'docs/market_prices/train'
+volatilities_from = "2024-10-18 00:00:00+01:00"
+volatilities_to = "2024-10-18 06:00:00+01:00"
+volatilities_filename = "docs/volatilities_train.pickle"
 
 # scrape data from training
 print("scraping finance data for training")
